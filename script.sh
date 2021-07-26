59a9634addf3df34190a68c03f14c0b5a7fc188a
logeshkumarsekar <51129349+logeshkumarsekar@users.noreply.github.com>
2021-07-26 18:25:52 +0530

    Add files via upload
    files

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+aws ecr wait image-scan-complete --repository-name loge-helloworld --image-id imageTag=LATEST
+if [ $(echo $?) -eq 0 ] ; then
+  SCAN_FINDINGS=$(aws ecr describe-image-scan-findings --repository-name $ECR_IMAGE_NAME --image-id imageTag=$IMAGE_TAG | jq '.imageScanFindings.findingSeverityCounts')
+  CRITICAL=$(echo $SCAN_FINDINGS | jq '.CRITICAL')
+  HIGH=$(echo $SCAN_FINDINGS | jq '.HIGH')
+  MEDIUM=$(echo $SCAN_FINDINGS | jq '.MEDIUM')
+  LOW=$(echo $SCAN_FINDINGS | jq '.LOW')
+  INFORMATIONAL=$(echo $SCAN_FINDINGS | jq '.INFORMATIONAL')
+  UNDEFINED=$(echo $SCAN_FINDINGS | jq '.UNDEFINED')
+  if [ $CRITICAL != null ] || [ $HIGH != null ] ; then
+    echo Docker image contains vulnerabilities at CRITICAL or HIGH level
+    aws ecr batch-delete-image --repository-name my-repo --image-ids imageTag=v0.1  #delete pushed image from container registry
+    exit 1  #exit execution due to docker image vulnerabilities
+  fi
+fi
